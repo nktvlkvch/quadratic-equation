0b77caf8a344b51e0f0329b499c39265814beb02
Nikita Valkevich <n.valkevich@icloud.com>
2018-09-30 20:52:30 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+ 	var arr = [],
+ 	myArray = equation.split(' '),
+ 	a = +myArray[0],
+ 	b = +(myArray[3] + myArray[4]),
+ 	c = +(myArray[7] + myArray[8]);
+ 	arr.push(Math.round((-1 * b) + Math.sqrt(b * b - 4 * a * c)) / (2 * a));
+	arr.push(Math.round((-1 * b) - Math.sqrt(b * b - 4 * a * c)) / (2 * a));
+	return arr.sort(function(x, y){return x - y});
+ 	
+
 }
+
